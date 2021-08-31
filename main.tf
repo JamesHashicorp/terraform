9373c7dd58d9101746014f16e168f99134243c0c
JamesHashicorp <jallen@hashicorp.com>
2021-08-31 19:31:02 +0100

    with lookup

--- main.tf
+++ main.tf
@@ -18,9 +18,24 @@ terraform {
 provider "aws" {
   region  = "us-west-2"
 }
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-08d70e59c07c61a3a"
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
 
   tags = {

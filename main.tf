be21f3a25e1ac6b3199e3d0d88b801d979ec2659
JamesHashicorp <jallen@hashicorp.com>
2021-08-31 19:44:26 +0100

    new region and ami

--- main.tf
+++ main.tf
@@ -16,11 +16,11 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region  = "us-east-1"
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-08d70e59c07c61a3a"
+  ami           = "ami-0c2b8ca1dad447f8a"
   instance_type = "t2.micro"
 
   tags = {

53a04301b96ee40178d5b429bf86a5733fdea75d
James <jallen@hashicorp.com>
2021-09-01 10:50:25 +0100

    addition of vm zone

--- main.tf
+++ main.tf
@@ -16,6 +16,7 @@ provider "google" {
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
   machine_type = "f1-micro"
+  zone         = "us-central1-c"
 
   boot_disk {
     initialize_params {

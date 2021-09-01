ad6781b4f88845004d6080122989e337b9d3bc35
James <jallen@hashicorp.com>
2021-09-01 11:02:48 +0100

    message change to code

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "google_compute_instance" "vm_instance" {
   }
 
   network_interface {
-    # A default network is created for all GCP projects.
+    # A default network is created for all GCP projects.Change Message
     network = google_compute_network.vpc_network.self_link
     access_config {
     }

0ddf8d24dfe52a9ef45000fd22a5e2ee50fc2b92
James <jallen@hashicorp.com>
2021-09-01 11:14:54 +0100

    name change

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "google" {
 }
 
 resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance1"
+  name         = "terraform-instance2"
   machine_type = "f1-micro"
   zone         = "us-east1-b"
 

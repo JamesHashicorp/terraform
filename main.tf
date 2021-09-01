6567f71d7c88c24234575401b1061e93be9550eb
James <jallen@hashicorp.com>
2021-09-01 10:43:32 +0100

    required provider

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
 provider "google" {
   project = "jameshashicorp"
   region  = "us-central1"
